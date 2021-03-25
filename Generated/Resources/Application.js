ed53ac744ec5df15b1688a0979df6039378a3a6f
Ramy Moustafa <Ramy.Moustafa@CNDC.LOCAL>
2021-03-25 13:34:42 +0200

    Create Soft Delete

---
+++ Generated/Resources/Application.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Application() {
+  return (
+    <React.Fragment>
+      <h1>Welcome</h1>
+    </React.Fragment>
+  );
+}
+
+export default Application;
---
+++ Generated/Resources/Master.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Application from './Application';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+    <React.Fragment>
+        <Application />
+    </React.Fragment>,
+    document.getElementById('Application')
+);
+
+reportWebVitals();
