9eff8c65d5ce05f5fbe676277c84790ea800301a
Ramy Moustafa <Ramy.Moustafa@CNDC.LOCAL>
2021-04-11 16:29:27 +0200

    Chnage in Routes

--- Resources/Routes/Conferences/ConferencesRoutes.js
+++ Resources/Routes/Conferences/ConferencesRoutes.js
@@ -9,21 +9,21 @@ import { VerifyToken } from '../../Middlewares/JWT/JWTMiddlewares';
 import { EndUserAdminToken, EndUserModeratorToken } from '../../Middlewares/EndUsers/EndUsersVerified';
 
 // Get All Conferences
-Route.get('/', [VerifyToken, EndUserAdminToken], Conferences.GetAllConferences);
+Route.get('/', VerifyToken, Conferences.GetAllConferences);
 
 // Create New Conference
-Route.post('/', Conferences.CreateNewConference);
+Route.post('/', [VerifyToken, EndUserAdminToken, EndUserModeratorToken], Conferences.CreateNewConference);
 
 // Get Conference By ID
-Route.get('/:ConferenceId', [VerifyToken, EndUserModeratorToken], Conferences.GetConferenceById);
+Route.get('/:ConferenceId', VerifyToken, Conferences.GetConferenceById);
 
 // Update Conference By ID
-Route.put('/:ConferenceId', Conferences.UpdateConferenceById);
+Route.put('/:ConferenceId', [VerifyToken, EndUserAdminToken], Conferences.UpdateConferenceById);
 
 // Soft Delete Conference By ID
-Route.delete('/:ConferenceId',  Conferences.SoftDeleteConferenceById);
+Route.delete('/:ConferenceId', [VerifyToken, EndUserAdminToken], Conferences.SoftDeleteConferenceById);
 
 // Force Delete Conference By ID
-Route.delete('/:ConferenceId',  Conferences.ForceDeleteConferenceById);
+Route.delete('/:ConferenceId/force-delete', [VerifyToken, EndUserAdminToken], Conferences.ForceDeleteConferenceById);
 
 export default Route;
